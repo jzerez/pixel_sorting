8df65d91021b3c056380a69e1ffbb2809359271a
jzerez <Jonathan.zerez@students.olin.edu>
2020-02-04 12:21:27 +0800

    creating backup branch for recursion intervals

--- image_utils.py
+++ image_utils.py
@@ -1,8 +1,11 @@
+import numpy as np
+from colorsys import rgb_to_hsv
 class Sorter:
     def __init__(self, image_path):
         im = Image.open(image_path)
         self.size = im.size
         self.im = np.asarray(im)
+        self.count = 0
 
     def calc_dist(self, target_pixel, neighboring_pixels):
         """
@@ -14,63 +17,103 @@ class Sorter:
             dists.append(dist)
         return dists
 
-    def is_in_frame(self, point, dim):
+    def is_in_frame(self, coor):
         """
         Checks to see if a coordinate is within the frame of the image
         """
-        return point >= 0 and point < self.size[dim]
+        return coor[0] >= 0 and coor[0] < self.size[0] and coor[1] >= 0 and coor[1] < self.size[1]
 
-    def get_chunk(self, root, queue, seen, dist_func):
+    def get_chunk(self, root, queue, seen, adj_func, filter_func):
         """
-        returns a chunk (group) of pixel coordinates that are adjacent to each other and satisfy a specified distance function
+        returns a chunk (group) of pixel coordinates that are adjacent to each other and satisfy a specified filter function:
+
+        what qualifies as adjacent is determined by the adj_func
+        what qualifies as acceptable is determined by the filter_func
         """
-        if not queue:
-            return seen
-        coor = queue.pop()
-        if dist_func(root, coor) and coor not in seen:
-            seen.add(coor)
-            queue.update(self.get_adj(coor))
-        return self.get_chunk(root, queue, seen, dist_func)
+        while queue:
+            self.count += 1
+            coor = queue.pop()
+            if filter_func(root, coor) and coor not in seen:
+                seen.add(coor)
+                adj = [adj for adj in adj_func(coor) if adj not in seen]
+                queue.update(adj)
+        return seen
+
+    # def threshold(self, lower_thresh, upper_thresh)
+
 
     def get_adj(self, coor):
+        """
+        return the up to eight adjacent coordinates to a given coordinate
+        """
         new_adj = []
         for i in [-1, 0, 1]:
-            new_x = coor[0] + i
-            if self.is_in_frame(new_x, 0):
-                for j in [-1, 0, 1]:
-                    if i == 0 and j == 0:
-                        continue
-                    new_y = coor[1] + j
-                    if self.is_in_frame(new_y, 1):
-                        new_adj.append((new_x, new_y))
+            for j in [-1, 0, 1]:
+                if i == 0 and j == 0:
+                    continue
+                new_coor = (coor[0] + i, coor[1] + j)
+
+                if self.is_in_frame(new_coor):
+                    new_adj.append(new_coor)
         return new_adj
 
+    def get_horiz_neighbors(self,coor):
+        """
+        return the up to two horizontal neighbors to a given coordinate
+        """
+        neighbors = []
+        for i in [-1, 1]:
+            new_coor = (coor[0]+i, coor[1])
+            if self.is_in_frame(new_coor):
+                neighbors.append(new_coor)
+        return neighbors
 
+    def get_vert_neighbors(self,coor):
+        """
+        return the up to two vertical neighbors to a given coordinate
+        """
+        neighbors = []
+        for j in [-1, 1]:
+            new_coor = (coor[0], coor[1]+j)
+            if self.is_in_frame(new_coor):
+                neighbors.append(new_coor)
+        return neighbors
+
+def lightness(pixel):
+    return rgb_to_hsv(pixel[0], pixel[1], pixel[2])[2] / 255.0
 
 if __name__ == "__main__":
     from PIL import Image
     import numpy as np
     import matplotlib.pyplot as plt
+    import pdb
+    import timeit
     im = Image.open('source/IMG_6129.jpg')
+    a = np.asarray(im)
+
     x,y = im.size
     r,g,b = im.split()
 
     s = Sorter('source/IMG_6129.jpg')
-    s.size=(10,10)
+
     print(s.get_adj((1,1)))
     print(s.get_adj((0,0)))
 
     def df(root, coor):
-        return np.linalg.norm(np.array(root)-np.array(coor)) < 4
+        return np.linalg.norm(np.array(root)-np.array(coor)) < 500
 
     r = (0,0)
-    seen = s.get_chunk(r, set([r]), set(), df)
-    print(seen)
-    plt.figure
-    for i in range(len(seen)):
-        coor = seen.pop()
-        plt.scatter(coor[0], coor[1], color='red')
-    plt.xlim((0, 10))
-    plt.ylim((0, 10))
-    plt.grid()
-    plt.show()
+    s.get_chunk(r, set([r]), set(), s.get_adj, df)
+    print(timeit.timeit(lambda: s.get_chunk(r, set([r]), set(), s.get_adj, df), number=10))
+
+    #
+    # print(len(seen))
+    # print(s.count)
+    # plt.figure
+    # for i in range(len(seen)):
+    #     coor = seen.pop()
+    #     plt.scatter(coor[0], coor[1], color='red')
+    # plt.xlim((0, 10))
+    # plt.ylim((0, 10))
+    # plt.grid()
+    # plt.show()
--- interval.py
+++ interval.py
@@ -6,23 +6,40 @@ import image_utils as util
 import pdb
 
 def threshold(im_array, size, lower_thresh, upper_thresh):
+    """
+    Divides the image into intervals based on lightness thresholds
+    returns: intervals
+
+        -intervals contains `n` lists, where `n` in the number of rows in the image
+        -each of the `n` lists contatins `x` lists, where `x` is the number of valid intervals within the row
+        -each of the `x` lists contains 2 numbers, the beginning and the end of the interval
+    """
+
     def in_thresh(val):
-        # pdb.set_trace()
+        """
+        returns true if a value is within the specificed threshold
+        """
         return val > lower_thresh and val < upper_thresh
-    # pdb.set_trace()
+
     intervals = []
+    # iterate through the rows (horizontal)
     for y in range(size[0]):
         intervals.append([])
+        # Whether to open a new interval for the first pixel or not
         open = in_thresh(util.lightness(im_array[y,0]))
+
         if open:
             intervals[y].append([0])
 
+        # Iterate through the pixels in the current row
         for x in range(size[1]):
             level = util.lightness(im_array[y,x])
             valid = in_thresh(level)
+            # If the interval is currently open, and the current pixel is not valid, end the current interval
             if open and not valid:
                 intervals[y][-1].append(x)
                 open = False
+            # If there isn't an interval currently open, and the current pixel is valid, open a new interval with the current pixel
             elif not open and valid:
                 intervals[y].append([x])
                 open = True
